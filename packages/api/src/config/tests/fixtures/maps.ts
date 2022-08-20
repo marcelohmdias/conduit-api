2755ed8ee92a35c9942d878968e3f6903e197d47
marcelohmdias <marcelohmdias@gmail.com>
2022-08-20 18:23:21 -0300

    refactor(api): :recycle: change mapAll type annotation

--- packages/api/src/config/tests/fixtures/maps.ts
+++ packages/api/src/config/tests/fixtures/maps.ts
@@ -3,8 +3,8 @@ import { pipe } from 'fp-ts/function'
 import { map, mapLeft, TaskEither } from 'fp-ts/lib/TaskEither'
 
 type Callback = Callable<unknown, unknown>
-
-type MapAll = (fn: Callback) => (data: TaskEither<unknown, unknown>) => TaskEither<unknown, unknown>
+type MapExecute = Callable<TaskEither<unknown, unknown>, TaskEither<unknown, unknown>>
+type MapAll = Callable<MapExecute, Callback>
 
 export const mapAll: MapAll = (fn) => (data) => {
   return pipe(data, map(fn), mapLeft(fn))
